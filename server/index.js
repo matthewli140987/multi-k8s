1f7dd855147947137e76870e4b73a187026468f9
matthewli1409 <matthewli1409@gmail.com>
2020-07-16 11:11:02 +0700

    init

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Postgres Setup
+// Postgres Client Setup
 const { Pool } = require('pg');
 const pgClient = new Pool({
   user: keys.pgUser,
@@ -18,13 +18,14 @@ const pgClient = new Pool({
   password: keys.pgPassword,
   port: keys.pgPort,
 });
+
 pgClient.on('connect', () => {
   pgClient
     .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
-// Redic Client Setup
+// Redis Client Setup
 const redis = require('redis');
 const redisClient = redis.createClient({
   host: keys.redisHost,
@@ -34,12 +35,14 @@ const redisClient = redis.createClient({
 const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
+
 app.get('/', (req, res) => {
   res.send('Hi');
 });
 
 app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * FROM values');
+  const values = await pgClient.query('SELECT * from values');
+
   res.send(values.rows);
 });
 
@@ -51,15 +54,18 @@ app.get('/values/current', async (req, res) => {
 
 app.post('/values', async (req, res) => {
   const index = req.body.index;
+
   if (parseInt(index) > 40) {
     return res.status(422).send('Index too high');
   }
+
   redisClient.hset('values', index, 'Nothing yet!');
   redisPublisher.publish('insert', index);
   pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
   res.send({ working: true });
 });
 
-app.listen(5000, () => {
+app.listen(5000, (err) => {
   console.log('Listening');
 });
